fc354718308560f6c05fcc5b3ea8d3b00bd646d1
Tasnimsomo <tasnasomo@gmail.com>
2023-11-28 09:41:48 -0800

    .8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    for i in str
+        if ord('a) <= ord(i) <= ord('z):
+            print("{:c}".format(ord(i) - 32), end='')
+        else:
+            print(c, end='')
+    print()
