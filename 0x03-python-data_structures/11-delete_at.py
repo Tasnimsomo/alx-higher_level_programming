c8217bdcaee7d5d74f0fac394168b3bba721849d
Tasnimsomo <tasnasomo@gmail.com>
2023-12-02 11:12:35 -0800

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,5 +4,6 @@ def delete_at(my_list=[], idx=0):
         return my_list
     else:
         for i, element in enumerate(my_list):
-            if idx == i
+            if idx == i:
                 my_list.remove(element)
+        return my_list
