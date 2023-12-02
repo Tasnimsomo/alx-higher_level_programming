e5d744428e6fe81856267c0c1ed0b326e0d690db
Tasnimsomo <tasnasomo@gmail.com>
2023-12-02 10:27:22 -0800

    .9-max_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for row in matrix:
+    for element in row:
+        print("{:d}".format(element), end=" ")
+        print()
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_number = 0
+    if my_list:
+        for i in my_list:
+            if i > max_number:
+                max_number = i
+        return (my_list)
+    else:
+        return None
