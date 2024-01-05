292156f04fcf2ce57440c6b0fdbd1ff6b4a30176
Tasnimsomo <tasnasomo@gmail.com>
2024-01-05 04:29:26 -0800

    0x09. Python - Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-i = 0
 def magic_string():
-    global i; i += 1; return "BestSchool" * i
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
