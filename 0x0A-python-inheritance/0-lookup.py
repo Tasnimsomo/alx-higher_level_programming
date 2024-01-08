c1aa5bfe28981537ec601cac3a4029644c8312cd
Tasnimsomo <tasnasomo@gmail.com>
2024-01-08 03:58:40 -0800

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that returns the list of available attributes"""
+
+
+def lookup(obj):
+    """function that returns the list of available attributes"""
+    return dir(obj)
