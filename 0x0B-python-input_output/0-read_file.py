b0f6639b8d5e8d1338301bf95c52424ed755fa21
Tasnimsomo <tasnasomo@gmail.com>
2024-01-09 03:35:38 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Write a function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Read the content of a file and print it to the console.
+
+    Parameters:
+    - filename (str): The name or path of the file to be read. Defaults to an empty string.
+
+    Returns:
+    None
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
