23f4d1660dbb0d79421f1f77578e68408c112794
Tasnimsomo <tasnasomo@gmail.com>
2024-01-09 08:25:38 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+
+def from_json_string(my_str):
+    """
+    Return a Python data structure represented by a JSON string.
+
+    Parameters:
+    - my_str (str): The JSON string to be converted.
+
+    Returns:
+    object: The Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
