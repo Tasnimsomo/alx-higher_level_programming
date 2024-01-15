b823a9d1712e9ecd3d9906deb67c49f531317259
Tasnimsomo <tasnasomo@gmail.com>
2024-01-15 12:26:12 -0800

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,7 +102,8 @@ class Rectangle(Base):
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                                                             self.id, self.__x,
-                                                            self.__y, self.__width,
+                                                            self.__y,
+                                                            self.__width,
                                                             self.__height)
 
     def update(self, *args, **kwargs):
