18bd3af9f7cc9c20f2fc8622105626b1b4a7f3c2
Tasnimsomo <tasnasomo@gmail.com>
2024-02-13 06:28:30 -0800

    0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
---script that lists all records with a score >= 10 in the table
+-- lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server
 -- Results should display both the score and the name (in this order)
 -- Records should be ordered by score (top first)
 -- The database name will be passed as an argument of the mysql command
