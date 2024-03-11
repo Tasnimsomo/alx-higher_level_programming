36d22bd885b8df4b352f8c31d5271703393e3ae0
root <root@DESKTOP-LPA9F3B>
2024-03-11 16:16:55 +0400

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const c = 'C is fun'
-const p = 'Python is cool'
-const j = 'JavaScript is amazing'
-console.log(c)
-console.log(p)
-console.log(j)
-
-
+const c = 'C is fun';
+const p = 'Python is cool';
+const j = 'JavaScript is amazing';
+console.log(c);
+console.log(p);
+console.log(j);
