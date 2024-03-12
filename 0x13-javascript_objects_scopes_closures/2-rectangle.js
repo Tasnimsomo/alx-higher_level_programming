b05cecc7f97ae0a1bf8444fe78839227b984dce7
root <root@DESKTOP-LPA9F3B>
2024-03-12 13:42:03 +0400

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,6 @@
 class Rectangle {
   constructor (w, h) {
       if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-	  return {};
-    } else {
       this.width = w;
       this.height = h;
     }
