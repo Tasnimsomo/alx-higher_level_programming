8d149af8520bb6bb0c01a7ffde8299b3f1fedcbd
root <root@DESKTOP-LPA9F3B>
2024-03-12 14:36:35 +0400

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,9 @@
-#!/usr/bin/node
 exports.converter = function (base) {
-	function convert (number) {
-		if (n < base) return n.tostring(base)
-		else {
-			return convert(Math.floor(number / base)) + (number % base).tostring(base);
+	return function convertToBase(n) {
+		if (n < base) {
+			return n.toString(base);
+		} else {
+			return convertToBase(Math.floor(n / base)) + (n % base).toString(base);
 		}
-		return convert;
-	};
+	}
+}
