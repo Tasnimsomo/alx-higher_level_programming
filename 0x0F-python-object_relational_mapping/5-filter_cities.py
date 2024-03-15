1906bbb23016a508fda7037dc0721250d686c099
root <root@DESKTOP-LPA9F3B>
2024-03-15 17:17:42 +0400

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cursor = connection.cursor()
     state_name = argv[4]
     query = "SELECT cities.id, cities.name FROM cities \
-            JOIN states ON cities.states_id = states.id \
+            JOIN states ON cities.state_id = states.id \
             WHERE states.name = %s \
             ORDER BY cities.id ASC"
     cursor.execute(query, (state_name,))
