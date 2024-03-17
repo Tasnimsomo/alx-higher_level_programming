32caed3b4eadb7eb7df6b7a8a4e5859e55a72fc9
root <root@DESKTOP-LPA9F3B>
2024-03-17 20:45:00 +0400

    cat 100-relationship_states_cities.sql | mysql -uroot -p

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
+"""
+Lists all State objects and corresponding City objects contained in the DB
+"""
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship
 from relationship_state import Base, State
 from relationship_city import City
-"""
-script that lists all State objects, and corresponding City objects, 
-contained in the database hbtn_0e_101_usa
-"""
 
 if __name__ == '__main__':
     engine = create_engine(
