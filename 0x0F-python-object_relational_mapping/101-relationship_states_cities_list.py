abbd4d487c488d3fb9144b52bb7f9fcda0f9c386
root <root@DESKTOP-LPA9F3B>
2024-03-17 20:58:17 +0400

    cat 100-relationship_states_cities.sql | mysql -uroot -p

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -13,7 +13,6 @@ from relationship_city import City
 if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     results = session.query(State).outerjoin(City).order_by(
