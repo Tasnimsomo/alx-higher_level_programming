a54d44452adb8b87d029b6615e459654df08e5ee
root <root@TasnimSomo>
2024-03-28 12:17:38 +0400

    0x11. Python - Network #1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,6 +6,6 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    response = requests.get('https://api.github.com/user', auth=(username, password))
-    data = response.json()
-    print(data['id'])
+    response = requests.get('https://api.github.com/user', auth=(
+        username, password))
+    print(response.json().get("id"))
