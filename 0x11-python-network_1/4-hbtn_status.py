1ca0d698b9d520051dcdff57d5c788bad2872ece
root <root@TasnimSomo>
2024-03-28 12:55:32 +0400

    0x11. Python - Network #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """4. What's my status? #1"""
-
 import requests
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response: ")
-    print("\t- type: ", type(response.text))
-    print("\t- content:", response.text)
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
