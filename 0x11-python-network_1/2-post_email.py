12def4b445fcff838e8825e788a0643f35064f6f
root <root@TasnimSomo>
2024-03-28 10:36:19 +0400

    0x11. Python - Network #1

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+import urllib.request
+import urllib.parse
+
+url = sys.argv[1]
+email = sys.argv[2]
+data = urllib.parse.urlencode({'email': email}).encode()
+
+request = urllib.request.Request(url, data=data, method='POST')
+
+with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
