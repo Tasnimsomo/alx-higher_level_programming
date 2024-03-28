4a37b98e399021445f814f0e3414a46e0cd3614e
root <root@TasnimSomo>
2024-03-28 12:13:17 +0400

    0x11. Python - Network #1

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""9. My GitHub!"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get('https://api.github.com/user', auth=(username, password))
+    data = response.json()
+    print(data['id'])
