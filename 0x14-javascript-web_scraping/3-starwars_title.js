eb6219180a870aa54d42f63ce3784b186c9f22d2
root <root@TasnimSomo>
2024-04-23 21:48:18 +0400

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,8 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const baseUrl = 'https://swapi-api.alx-tools.com/api/films/${movieId}';
+const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const url = baseUrl + movieId;
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
