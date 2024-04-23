2bee136626eeb0691105860e88485e9683f3ce7c
root <root@TasnimSomo>
2024-04-23 22:26:25 +0400

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
-const characterId = 18;
+const url = process.argv[2];
+const characterId = '18';
+let count = 0;
 
-request.get(baseUrl, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+    console.log(count);
   }
-  const films = JSON.parse(body);
-  let counter = 0;
-  for (const film of films.results) {
-    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-      counter++;
-    }
-  }
-
-  console.log(counter);
 });
